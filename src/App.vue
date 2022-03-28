bf5f86c76729e15cdcb2f0eabc2157a2743cc212
Growl2u <dwulfson@comcast.net>
2022-03-27 23:49:11 -0500

    updated heroku branch with currently deployed files

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import Superfrog from './components/Superfrog.vue';
+export default {
+  components: { Superfrog },
+};
+</script>
+
+<template>
+  <Superfrog></Superfrog>
+</template>
+
+<style>
+</style>
