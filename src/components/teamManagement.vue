0c825a7f3108c63f8d776d21eabc748ab4648130
Samuel Callan <samuelcallan@Samuels-MacBook-Pro-2.local>
2022-03-30 16:49:10 -0500

    adding modal to teamManagement

--- src/components/teamManagement.vue
+++ src/components/teamManagement.vue
@@ -1,16 +1,41 @@
 <template>
-<link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-/>
 <div>
     <div class="container">
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">SuperFrog Team Management</h5>
-                <button type="button" class="btn btn-success">Add SuperFrog Student</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Add SuperFrog Student</button>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form >
+                                <div class="form-group">
+                                    <label for="">Student Name</label>
+                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                    <small id="exampleInputEmail1" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Student Address</label>
+                                    <input type="text" class="form-control" id="address" placeholder="Enter address">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <table class="table">
@@ -25,7 +50,7 @@
             </tr>
             </thead>
             <tbody id="userTableBody">
-                <tr v-for="user in teamlist">
+                <tr v-for="user in teamlist" :key="user.id">
                     <th scope="row" v-if="user.isActive"> {{user.id}}</th>
                     <td v-if="user.isActive"> {{user.name}}  </td>
                     <td v-if="user.isActive"> {{user.email}}  </td>
@@ -62,4 +87,7 @@ thead tr{
     color: white;
 }
 
+.modal-body{
+    color: black;
+}
 </style>
