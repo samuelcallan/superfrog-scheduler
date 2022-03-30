131122e97e68ffedff25b815bcd1e23deb56c999
Samuel Callan <samuelcallan@Samuels-MacBook-Pro-2.local>
2022-03-30 16:50:28 -0500

    customer

--- src/components/customerManagement.vue
+++ src/components/customerManagement.vue
@@ -1,10 +1,4 @@
 <template>
-<link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-/>
 <div>
     <div class="container">
         <div class="card">
@@ -24,7 +18,7 @@
             </tr>
             </thead>
             <tbody id="userTableBody">
-                <tr v-for="user in userlist">
+                <tr v-for="user in userlist" :key="user.id">
                     <th scope="row" v-if="user.isActive"> {{user.id}}</th>
                     <td v-if="user.isActive"> {{user.name}}  </td>
                     <td v-if="user.isActive"> {{user.email}}  </td>
