efc0027533e1f544dd4b6618855affa6674bcf5a
ZylerPierceNiece <102488020+ZylerPierceNiece@users.noreply.github.com>
2022-04-30 16:17:48 -0500

    Update customerManagement.vue

--- src/components/customerManagement.vue
+++ src/components/customerManagement.vue
@@ -4,6 +4,53 @@
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Customer Management</h5>
+                <!-- Edit a user dialog -->
+    <div class="modal fade" id="editUserDialog" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Edit a User</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group row">
+                <label for="username-edit" class="col-sm-2 col-form-label">Name</label>
+                <div class="col-sm-10">
+                  <input type="text" readonly class="form-control-plaintext" id="username-edit" value="" />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="email-edit" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="email-edit" value="" />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="bookingdate-edit" class="col-sm-2 col-form-label">Booking Date</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="bookingdate-edit" value="" />
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="address-edit" class="col-sm-2 col-form-label">Address</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="address-edit" value="" />
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary" onclick="updateUser()">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
             </div>
         </div>
         <table class="table">
@@ -27,6 +74,9 @@
                     <td> {{user.profile.result.bookingDate}} </td>
                     <td> {{user.profile.result.isActive}} </td>
                     <td> {{user.profile.result.address}} </td>
+                    <td> 
+                        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="editUserDialog" @click="showEditDialog(user.id)">Edit</button>
+                    </td>
                     <td>
                         <button type="button" class="btn btn-danger" @click="deleteApp(user.id)">Remove</button>
                     </td>
@@ -45,13 +95,20 @@ import axios from "axios";
 export default {
     data(){
         return{
-            userlist: []
+            userlist: [],
+            editForm: {
+                id: '',
+                name: '',
+                email: '',
+                bookingDate: '',
+                address: '',
+            },
         }
     },
     mounted(){
         this.retrieve();
     },
-    // props: ['userlist'],
+    props: ['userlist', 'editForm'],
     methods: {
         deleteApp(id) {
             for(let i = 0; i < this.userlist.length; i++){
@@ -73,7 +130,41 @@ export default {
             .then(response => (
                 this.userlist = response.data
             ))
-        }
+        },
+        showEditDialog(userId) {
+            var name = document.getElementById("username-edit");
+            var email = document.getElementById("email-edit");
+            var bookingDate = document.getElementById("bookingdate-edit");
+            var address = document.getElementById('address-edit');
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].id == userId) {
+                    email.value = data[i].email;
+                    name.value = data[i].name;
+                    bookingDate.value = data[i].bookingDate;
+                    address.value = data[i].address;
+
+                }
+            }
+            editForm.id = userId;
+         },
+        updateUser() {
+            var email = document.getElementById("email-edit");
+            var bookingDate = document.getElementById("bookingdate-edit");
+            var address = document.getElementById('address-edit');
+            var userId = editForm.id;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].id == userId) {
+                    data[i].email = email.value;
+                    data[i].bookingDate = bookingDate.value;
+                    data[i].address = address.value;
+                }
+            }
+            editForm.id = '';
+            editForm.name = '';
+            editForm.email = '';
+            editForm.bookingDate = '';
+            editForm.address = '';
+        },
     }
 };
 </script>
