cade3cb72740dc71d28a7e388d81d9f2fac4c44f
Samuel Callan <samuelcallan@Samuels-MacBook-Pro-2.local>
2022-05-01 13:55:36 -0500

    deploy

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -3,7 +3,7 @@ const info = async () => {
 };
 
 const login = async (loginInfo) => {
-  return await fetch('https://superfrog-scheduler-frontend.herokuapp.com/api/auth/login', {
+  return await fetch('localhost:4000/api/auth/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
