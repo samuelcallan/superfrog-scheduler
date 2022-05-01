56deb73168448cfa3e286cc6a2b9df4e1ec9382b
Samuel Callan <samuelcallan@Samuels-MacBook-Pro-2.local>
2022-05-01 13:04:37 -0500

    deploy

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import router, { setupRouter } from '@/router';
 import { setupPlugins } from './plugins';
 
 
-const app = createApp(App);
+async function bootstrap() {
+  const app = createApp(App);
   setupPlugins(app); // since router will use Pinia, we must set up Pinia first
   setupRouter(app);
-  
-  async function bootstrap(){
-    await router.isReady(); // wait till router is ready before mounting
-    app.mount('#app');
-  }
 
-  bootstrap();
+  await router.isReady(); // wait till router is ready before mounting
+  app.mount('#app');
+}
+
+bootstrap();
   
 
 
