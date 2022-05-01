557205508f17ba5264fb2bd463784ee9c2f5883b
Samuel Callan <samuelcallan@Samuels-MacBook-Pro-2.local>
2022-05-01 14:52:49 -0500

    deploy

--- src/apis/userApi.js
+++ src/apis/userApi.js
@@ -3,7 +3,7 @@ const info = async () => {
 };
 
 const login = async (loginInfo) => {
-  return await fetch('https://superfrog-scheduler-frontend.herokuapp.com', {
+  return await fetch('https://superfrog-scheduler-frontend.herokuapp.com/api/auth/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
